e8d95fd8350a5ef5fd9197fcc976b9eda82d0247
ImageSlipDemo <945152947@qq.com>
2016-12-02 09:25:24 +0800

    eidt from github

--- app/src/main/java/com/fr/here/test/RxJavaBuffer.java
+++ app/src/main/java/com/fr/here/test/RxJavaBuffer.java
@@ -17,9 +17,6 @@ public class RxJavaBuffer {
         List<String> list = new ArrayList<String>();
         list.add("1");
         list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add("5");
         list.add("6");
         list.add("7");
         list.add("8");
